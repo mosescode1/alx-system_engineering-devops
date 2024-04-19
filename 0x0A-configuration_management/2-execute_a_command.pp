741867a34c000a34c113740b9c273b67c8ee918b
mosescode1 <emosescode@gmail.com>
2024-04-19 19:47:27 +0100

    fixed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# Using Puppet, install flask from pip3.
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  onlyif      => 'pgrep killmenow'
+# Create a Puppet manifest to kill a process named "killmenow" using pkil
+
+exec { 'kill_killmenow_process':
+  command     => '/usr/bin/pkill -f /alx-system_engineering-devops/0x0A-configuration_management/killmenow',
+  onlyif      => '/usr/bin/pgrep -f /alx-system_engineering-devops/0x0A-configuration_management/killmenow', # Check if the process is running before attempting to kill it
   refreshonly => true,
 }
