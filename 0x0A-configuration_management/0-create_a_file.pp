27fcb129294f5cc6e9199f904920a26b7fd7947d
mosescode1 <emosescode@gmail.com>
2024-04-19 14:50:49 +0100

    puppet lint

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# creating a file in /tmp
+$file_path = '/tmp/school'
+
+file { $file_path:
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  contain => 'I Love Puppet',
+}
+
