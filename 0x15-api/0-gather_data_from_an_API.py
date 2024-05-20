b81e54db83485f9f4df92f10202d80c3e733805e
mosescode1 <emosescode@gmail.com>
2024-05-20 15:43:22 +0100

    First api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+id = sys.argv[1]
+
+api_url_user_name = f"https://jsonplaceholder.typicode.com/users/{id}"
+user_name_res = requests.get(api_url_user_name).json()
+
+api_url_todos = f"{api_url_user_name}/todos"
+
+user_todo = requests.get(api_url_todos).json()
+
+emp_name = user_name_res.get('name')
+
+total_task = 0
+completed = 0
+
+for val in user_todo:
+    total_task += 1
+    if val.get('completed'):
+        completed += 1
+
+print(
+    f"Employee {emp_name} is done with tasks({completed}/{total_task}):")
+
+
+for val in user_todo:
+    print(f"\t {val['title']}")
