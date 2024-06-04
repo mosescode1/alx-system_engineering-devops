fffabefe19e2ba6c90ec2d706b71dea16a49cfc4
mosescode1 <emosescode@gmail.com>
2024-06-04 23:11:41 +0100

    tryagain

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,13 @@
 """Module fires a request to Reddit API
 and returns the number of subscribers
 """
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """Function returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    response = get(url)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     return 0
